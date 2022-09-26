5bb24c047b5b9701e7207a8dbf03032dd39b4d84
Ricardo Reyes Flores <ricardoreyesflores2016@outlook.com>
2022-09-26 09:42:02 -0500

    Clase 77

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 const express = require("express")
+const cors = require ("cors")
 
 const app = express()
+
+app.use(cors())
+app.use(express.json())
 
 const jugadores = []
 class Jugador{
     constructor(id){
         this.id = id
+    }
+    asignarMokepon(mokepon){
+        this.mokepon = mokepon
+    }
+}
+
+class Mokepon {
+    constructor(nombre){
+        this.nombre = nombre
     }
 }
 
@@ -16,6 +29,19 @@ app.get("/unirse",(req, res)=>{
     jugadores.push(jugador)
     res.setHeader("Access-Control-Allow-Origin","*")
     res.send(id)
+})
+
+app.post("/mokepon/:jugadorId",(req,res)=> {
+    const jugadorId = req.params.jugadorId || ""
+    const nombre = req.body.mokepon || ""
+    const mokepon = new Mokepon (nombre)
+    const jugadorIndex = jugadores.findIndex((jugador) => jugadorId === jugador.id)
+    if (jugadorIndex >= 0){
+        jugadores[jugadorIndex].asignarMokepon(mokepon)
+    }
+    console.log(jugadores)
+    console.log(jugadorId)
+    res.end
 })
 
 app.listen(8080,()=>{
--- js/mokepon.js
+++ js/mokepon.js
@@ -23,6 +23,7 @@ const mapa = document.getElementById('mapa')
 
 const anchoMaximoDelMapa = 550
 
+let jugadorId = null
 let mokepones = []
 let ataqueJugador = []
 let ataqueEnemigo = []
@@ -185,6 +186,7 @@ function unirseAlJuego() {
                 res.text()
                     .then(function (respuesta) {
                         console.log(respuesta)
+                        jugadorId = respuesta
                     })
             }
         })
@@ -207,9 +209,23 @@ function seleccionarMascotaJugador() {
         alert('Selecciona una mascota')
     }
 
+    seleccionarMokepon(mascotaJugador);
+
     extraerAtaques(mascotaJugador)
     sectionVerMapa.style.display = 'flex'
     iniciarMapa()
+}
+
+function seleccionarMokepon(mascotaJugador){
+    fetch(` http://localhost:8080/mokepon/${jugadorId}`,
+    {method: "post",
+    headers:{
+        "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+        mokepon: mascotaJugador
+    })
+})
 }
 
 function extraerAtaques(mascotaJugador) {
